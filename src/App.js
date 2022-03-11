4cc6990b2a5205edf1129d81d2494dea47e29ce6
chanaka <chanaka@orelit.com>
2022-03-11 09:58:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import { useDetectOutsideClick } from "./component/useDetectOutsideClick";
+import "./App.css";
+import logo from './user.png'
 
 function App() {
+  const dropdownRef = useRef(null);
+  const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
+  const onClick = () => setIsActive(!isActive);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container">
+      <div className="action">
+        <button onClick={onClick} className="profile">
+          <img
+            src={logo}
+            alt="User avatar"
+          />
+        </button>
+        <nav
+          ref={dropdownRef}
+          className={`menu ${isActive ? "active" : "inactive"}`}
         >
-          Learn React
-        </a>
-      </header>
+           <h3>
+                User Account
+                <div>
+                    Operational Team
+                </div>
+            </h3>
+          <ul>
+            <li>
+              <span class="material-icons icons-size"></span>
+              <a href="#">My Profile</a>
+            </li>
+            <li>
+              <span class="material-icons icons-size"></span>
+              <a href="#">Edit Account</a>
+            </li>
+            <li>
+              <span class="material-icons icons-size"></span>
+              <a href="#">Comments</a>
+            </li>
+            <li>
+              <span class="material-icons icons-size"></span>
+              <a href="#">Budget</a>
+            </li>
+            <li>
+              <span class="material-icons icons-size"></span>
+              <a href="#">Finance</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
